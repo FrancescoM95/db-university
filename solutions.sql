d55fa64fb0bdda1ed86342bd03c4cbcd908dff7e
FrancescoM95 <mon95fran@gmail.com>
2024-03-01 15:23:04 +0100

    group 1

--- solutions.sql
+++ solutions.sql
@@ -1,3 +1,5 @@
+--------------------------# QUERY SELECT #--------------------------
+
 --* 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
 SELECT * 
@@ -53,4 +55,26 @@ FROM `departments`;
 SELECT * 
 FROM `teachers`
 WHERE `phone` IS NULL;
+
+
+--------------------------# QUERY GROUP #--------------------------
+
+--* 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(*) as `studenti_iscritti`, YEAR(`enrolment_date`)
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+
+--* 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+
+
+
+--* 3. Calcolare la media dei voti di ogni appello d'esame
+
+
+
+
+--* 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 
